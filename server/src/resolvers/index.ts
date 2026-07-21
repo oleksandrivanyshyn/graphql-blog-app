8566605745a78ec227d52c85174210d957a03650
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-21 17:57:06 +0300

    feat: add Post and User resolvers for relational data handling in GraphQL schema

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,7 +1,12 @@
 import { Query } from './Query';
 import { Mutation } from './Mutation/Mutation';
+import { Post } from './Post';
+import { Profile } from './Profile';
+import { User } from './User';
 export const resolvers = {
   Query,
   Mutation,
   Profile,
+  Post,
+  User,
 };
