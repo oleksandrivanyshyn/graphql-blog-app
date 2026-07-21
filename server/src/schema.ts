b2d7d33830b908746c2499e1152cc78929546bf3
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-21 16:58:20 +0300

    feat: add postDelete resolver and update postUpdate input arguments

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -6,7 +6,8 @@ export const typeDefs = gql`
   }
   type Mutation {
     postCreate(post: PostInput!): PostPayload!
-    postUpdate(post: PostInput!): PostPayload!
+    postUpdate(postId: ID!, post: PostInput!): PostPayload!
+    postDelete(postId: ID!): PostPayload!
   }
   type Post {
     id: ID!
